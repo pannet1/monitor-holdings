5aa3e5be1f8f9741de365b0a261adab7dfa28a89
karthick <prog@ecomsense.in>
2023-08-25 13:34:36 +0530

    add t1_holdings into the calculations

--- monitor_holdings/main.py
+++ monitor_holdings/main.py
@@ -10,27 +10,33 @@ dir_path = "../../../"
 logging = Logger(10)
 fileutils = Fileutils()
 holdings = dir_path + "holdings.csv"
-
+settings_file = "settings.yaml"
 
 try:
-    settings = fileutils.get_lst_fm_yml("settings.yaml")
+    logging.debug(f"read settings from {settings_file}")
+    settings = fileutils.get_lst_fm_yml(settings_file)
     perc = settings['perc']
     buff = settings['buff']
     perc_col_name = f"perc_gr_{int(perc)}"
+    logging.debug("create a column named after user settings {perc_col_name}")
     broker = get_kite(api="bypass", sec_dir=dir_path)
-    logging.info("getting holdings for the day ...")
+    logging.debug("getting holdings for the day ...")
     resp = broker.kite.holdings()
     df = pd.DataFrame(resp)
     selected_cols = ['tradingsymbol', 'exchange', 'instrument_token',
-                     'realised_quantity', 'close_price', 'average_price', 'pnl']
+                     'quantity', 't1_quantity', 'close_price', 'average_price', 'pnl']
+    logging.debug(f"filtering columns {str(selected_cols)}")
     df = df[selected_cols]
-    df['cap'] = (df['realised_quantity'] * df['average_price']).astype(int)
+    logging.debug("applying necessary rules ...")
+    df['calculated'] = df['quantity'] + df['t1_quantity']
+    df['cap'] = (df['calculated'] * df['average_price']).astype(int)
     df['perc'] = ((df['pnl'] / df['cap']) * 100).round(2)
     df['perc'] = ((df['pnl'] / df['cap']) * 100).where((df['cap']
                                                         != 0) & (df['pnl'] != 0), 0).round(2)
     cond = f"perc > {perc}"
     df[perc_col_name] = df.eval(cond)
     print(df)
+    logging.debug(f"writing to csv ... {holdings}")
     df.to_csv(holdings, index=False)
 except Exception as e:
     remove_token(dir_path)
@@ -40,10 +46,12 @@ except Exception as e:
 
 try:
     lst = []
+    logging.debug(f"reading from csv ...{holdings}")
     df = pd.read_csv(holdings)
+    logging.debug("filtering for required columns")
     df['key'] = df['exchange'] + ":" + df['tradingsymbol']
     df.set_index('key', inplace=True)
-    df = df[~(df == False).any(axis=1)]
+    df = df[df[perc_col_name] != False]
     df.drop(['exchange', 'tradingsymbol', 'average_price',
             'pnl', 'cap', perc_col_name], axis=1, inplace=True)
     lst = df.index.to_list()
@@ -66,7 +74,7 @@ def order_place(index, row):
             tradingsymbol=exchsym[1],
             exchange=exchsym[0],
             transaction_type='SELL',
-            quantity=int(row['realised_quantity']),
+            quantity=int(row['calculated']),
             order_type='LIMIT',
             product='CNC',
             variety='regular',
@@ -86,6 +94,8 @@ def order_place(index, row):
 
 
 try:
+    logging.debug("loop for LTP to check conditions and exit holdings")
+    logging.debug("are we having any holdings to check")
     while len(lst) > 0:
         resp = broker.kite.ohlc(lst)
         dct = {k: {'ltp': v['last_price'], 'high': v['ohlc']['high']}
@@ -100,12 +110,11 @@ try:
                 if is_placed:
                     rows_to_remove.append(index)
 
-        # Remove rows based on the indices collected
         df.drop(rows_to_remove, inplace=True)
         print(df, "\n")
         sleep(settings['secs'])
     else:
-        print("there is no holdings to process ... exiting")
+        logging.info("there is no holdings to process ... exiting")
         sys.exit(0)
 except Exception as e:
     remove_token(dir_path)
