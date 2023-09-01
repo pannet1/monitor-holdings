d5ae45414ab8423ce1a532c78b892b245b508fd1
karthick <prog@ecomsense.in>
2023-09-01 10:30:41 +0530

    add nice label to trendyline

--- monitor_holdings/buy.py
+++ monitor_holdings/buy.py
@@ -44,7 +44,8 @@ try:
     lst_tlyne = []
     lst_dct_tlyne = Trendlyne().entry()
     if lst_dct_tlyne and any(lst_dct_tlyne):
-        logging.debug(f"stocks returned by trendlyne {lst_dct_tlyne}")
+        print(pd.DataFrame(
+            lst_dct_tlyne).set_index('tradingsymbol').rename_axis('Trendlyne'), "\n")
         lst_tlyne = [dct['tradingsymbol'] for dct in lst_dct_tlyne]
 except Exception as e:
     print(traceback.format_exc())
@@ -53,10 +54,10 @@ except Exception as e:
 
 try:
     if any(lst_tlyne):
-        logging.debug(f"reading trendlyne ...{lst_tlyne}")
+        logging.info(f"reading trendlyne ...{lst_tlyne}")
         lst_tlyne = [
             x for x in lst_tlyne if x not in lst]
-        logging.debug(f"filtered from holdings: {lst}")
+        logging.info(f"filtered from holdings: {lst}")
 
         # get list from positions
         lst_dct = broker.positions
@@ -64,7 +65,7 @@ try:
             lst = [dct['symbol'] for dct in lst_dct]
             lst_tlyne = [
                 x for x in lst_tlyne if x not in lst]
-            logging.debug(f"filtered from positions ...{lst}")
+            logging.info(f"filtered from positions ...{lst}")
 except Exception as e:
     print(traceback.format_exc())
     logging.error(f"{str(e)} unable to read positions")
