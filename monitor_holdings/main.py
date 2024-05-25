f326466f58247ffddb2e99b05ab6618c2f9a8c7b
pannet1 <prog@ecomsense.in>
2024-05-26 00:28:22 +0530

    remove global scopoe

--- monitor_holdings/main.py
+++ monitor_holdings/main.py
@@ -6,44 +6,39 @@ from traceback import print_exc
 from constants import logging, dir_path, buff, secs, perc_col_name
 from holdings import get
 
-try:
-    broker = get_kite()
-    logging.debug("getting holdings for the day ...")
-    resp = broker.kite.holdings()
-    df = get(resp)
-except Exception as e:
-    remove_token(dir_path)
-    print_exc()
-    logging.error(f"{str(e)} unable to get holdings")
-    sys.exit(1)
 
-try:
-    lst = []
-    logging.debug("filtering for required columns")
-    df["key"] = df["exchange"] + ":" + df["tradingsymbol"]
-    df.set_index("key", inplace=True)
-    df = df[df[perc_col_name] != False]
-    df.drop(
-        [
-            "exchange",
-            "tradingsymbol",
-            "average_price",
-            "last_price",
-            "unrealized",
-            "cap",
-            perc_col_name,
-        ],
-        axis=1,
-        inplace=True,
-    )
-    lst = df.index.to_list()
-except Exception as e:
-    print_exc()
-    logging.error(f"{str(e)} while reading from csv")
-    sys.exit(1)
+def init():
+    try:
+        broker = get_kite()
+        logging.debug("getting holdings for the day ...")
+        resp = broker.kite.holdings()
+        df = get(resp)
+        logging.debug("filtering for required columns")
+        df["key"] = df["exchange"] + ":" + df["tradingsymbol"]
+        df.set_index("key", inplace=True)
+        df = df[df[perc_col_name] != False]
+        df.drop(
+            [
+                "exchange",
+                "tradingsymbol",
+                "average_price",
+                "last_price",
+                "unrealized",
+                "cap",
+                perc_col_name,
+            ],
+            axis=1,
+            inplace=True,
+        )
+    except Exception as e:
+        print_exc()
+        logging.error(f"{str(e)} while reading from csv")
+        sys.exit(1)
+    else:
+        return broker, df
 
 
-def order_place(index, row):
+def order_place(index, row, broker):
     try:
         exchsym = index.split(":")
         logging.info(f"placing order for {index}, str{row}")
@@ -69,39 +64,50 @@ def order_place(index, row):
         return False
 
 
-try:
-    logging.debug("loop for LTP to check conditions and exit holdings")
-    logging.debug("are we having any holdings to check")
-    while len(lst) > 0:
-        resp = broker.kite.ohlc(lst)
-        dct = {
-            k: {
-                "ltp": v["last_price"],
-                "open": v["ohlc"]["open"],
-                "high": v["ohlc"]["high"],
-                "low": v["ohlc"]["low"],
+def run(broker, df):
+    try:
+        logging.debug("loop for LTP to check conditions and exit holdings")
+        logging.debug("are we having any holdings to check")
+        lst = []
+        lst = df.index.to_list()
+        while len(lst) > 0:
+            resp = broker.kite.ohlc(lst)
+            dct = {
+                k: {
+                    "ltp": v["last_price"],
+                    "open": v["ohlc"]["open"],
+                    "high": v["ohlc"]["high"],
+                    "low": v["ohlc"]["low"],
+                }
+                for k, v in resp.items()
             }
-            for k, v in resp.items()
-        }
-        df["ltp"] = df.index.map(lambda x: dct[x]["ltp"])
-        df["open"] = df.index.map(lambda x: dct[x]["open"])
-        df["high"] = df.index.map(lambda x: dct[x]["high"])
-        df["low"] = df.index.map(lambda x: dct[x]["low"])
+            df["ltp"] = df.index.map(lambda x: dct[x]["ltp"])
+            df["open"] = df.index.map(lambda x: dct[x]["open"])
+            df["high"] = df.index.map(lambda x: dct[x]["high"])
+            df["low"] = df.index.map(lambda x: dct[x]["low"])
 
-        rows_to_remove = []
-        for index, row in df.iterrows():
-            if row["high"] > row["close_price"] and row["ltp"] < row["close_price"]:
-                is_placed = order_place(index, row)
-                if is_placed:
-                    rows_to_remove.append(index)
+            rows_to_remove = []
+            for index, row in df.iterrows():
+                if row["high"] > row["close_price"] and row["ltp"] < row["close_price"]:
+                    is_placed = order_place(index, row, broker)
+                    if is_placed:
+                        rows_to_remove.append(index)
 
-        df.drop(rows_to_remove, inplace=True)
-        print(df, "\n")
-        sleep(secs)
-    else:
-        logging.info("there is no holdings to process ... exiting")
-        sys.exit(0)
-except Exception as e:
-    remove_token(dir_path)
-    print_exc()
-    logging.error(f"{str(e)} in the main loop")
+            df.drop(rows_to_remove, inplace=True)
+            print(df, "\n")
+            sleep(secs)
+        else:
+            logging.info("there is no holdings to process ... exiting")
+            sys.exit(0)
+    except Exception as e:
+        remove_token(dir_path)
+        print_exc()
+        logging.error(f"{str(e)} in the main loop")
+
+
+def main():
+    api, df = init()
+    run(api, df)
+
+
+main()
