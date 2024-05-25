8e5de97ed1edb5ae387a8d97c59c82c1e53b1faf
pannet1 <prog@ecomsense.in>
2024-05-25 21:58:09 +0530

    change requirements

--- monitor_holdings/login_get_kite.py
+++ monitor_holdings/login_get_kite.py
@@ -9,7 +9,6 @@ def get_kite():
     else:
         print("trying login ZERODHA ..")
         kite = _get_zerodha()
-        # kite = Zerodha()
     return kite
 
 
@@ -70,99 +69,6 @@ def remove_token(dir_path):
     dct = CNFG["bypass"]
     tokpath = dir_path + dct["userid"] + ".txt"
     os.remove(tokpath)
-
-
-def Zerodha():
-    import requests
-    import pyotp
-    from kiteconnect import KiteConnect
-    import sys
-
-    dct = CNFG["zerodha"]
-    logging.debug(dct)
-    user_id = dct["userid"]
-    password = dct["password"]
-    totp = dct["totp"]
-    api_key = dct["api_key"]
-    secret = dct["secret"]
-    tokpath = dir_path + dct["userid"] + ".txt"
-
-    LOGINURL = "https://kite.zerodha.com/api/login"
-    TWOFAURL = "https://kite.zerodha.com/api/twofa"
-
-    try:
-        session = requests.Session()
-        session_post = session.post(
-            LOGINURL, data={"user_id": user_id, "password": password}
-        ).json()
-        logging.debug(f"{session_post=}")
-        if (
-            session_post
-            and isinstance(session_post, dict)
-            and session_post["data"].get("request_id", False)
-        ):
-            request_id = session_post["data"]["request_id"]
-            logging.debug(f"{request_id=}")
-        else:
-            raise ValueError("Request id is not found")
-    except ValueError as ve:
-        logging.error(f"ValueError: {ve}")
-        sys.exit(1)  # Exit with a non-zero status code to indicate an error
-    except requests.RequestException as re:
-        logging.error(f"RequestException: {re}")
-        sys.exit(1)
-    except Exception as e:
-        # Handle other unexpected exceptions
-        logging.exception(f"An unexpected error occurred: {e}")
-        sys.exit(1)
-
-    try:
-        data = {
-            "user_id": user_id,
-            "request_id": request_id,
-            "twofa_value": pyotp.TOTP(totp).now(),
-            "skip_session": True,
-        }
-        response = session.post(TWOFAURL, data=data, allow_redirects=True)
-        response.raise_for_status()  # Raise an exception for HTTP errors
-    except Exception as e:
-        logging.exception(f"twofa error: {e}")
-        sys.exit(1)
-
-    try:
-        data = {"api_key": api_key, "allow_redirects": True}
-        session_get = session.get("https://kite.trade/connect/login/", params=data)
-        response.raise_for_status()  # Raise an exception for HTTP errors
-    except Exception as e:
-        e = str(e)
-        logging.debug(f"{e=}")
-        if "request_token" in e:
-            request_token = e.split("request_token=")[1].split(" ")[0]
-            logging.debug(f"{request_token=}")
-            pass
-        else:
-            logging.exception(f"request token error: {e}")
-            sys.exit(1)
-    else:
-        split_url = session_get.url.split("request_token=")
-        if len(split_url) >= 2:
-            request_token = split_url[1].split("&")[0]
-
-    try:
-        kite = KiteConnect(api_key=api_key)
-        logging.debug(f"{data=} generated with the {request_token=}")
-        data = kite.generate_session(request_token, api_secret=secret)
-        if data and isinstance(data, dict) and data.get("access_token", False):
-            logging.debug(f"{data['access_token']}")
-            with open(tokpath, "w") as tok:
-                tok.write(data["access_token"])
-            return kite
-        else:
-            raise ValueError("Unable to generate session")
-    except Exception as e:
-        # Handle any unexpected exceptions
-        logging.exception(f"generating session error: {e}")
-        sys.exit(1)
 
 
 if __name__ == "__main__":
